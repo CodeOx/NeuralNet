2efdc0c6715f0a9e2e16132e0bc11ae3d062c4f1
Chetan Mittal <mittalchetan98@gmail.com>
2019-04-07 18:11:13 +0530

    cleaned code

--- neural_net.py
+++ neural_net.py
@@ -12,15 +12,6 @@ from sklearn import preprocessing
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
-# input : a vector [[a1,a2,a3,...]]
-# returns : D = [[a1(1-a1),0,0,0,..], [0,a2(1-a2),0,0,..], ...]
-def convert_vector_to_D(input):
-	one_minus_input = 1 - input
-	product = input * one_minus_input
-	identity = np.eye(len(input))
-	D = identity * product
-	return D
-
 # input : parameters for a neural network
 # returns : empty neural net, i.e., a list of weight matrices
 def create_network(num_inputs, hidden_units_list, num_output):
@@ -33,31 +24,12 @@ def create_network(num_inputs, hidden_units_list, num_output):
 		units_right_layer = hidden_units_list[i]
 		
 		w[i] = np.random.normal(loc=float(0), scale=std, size=(units_left_layer + 1, units_right_layer))	# + 1 for bias
-		#w[i] = np.random.uniform(-0.1, 0.1, (units_left_layer + 1, units_right_layer))	# + 1 for bias
 		
 		units_left_layer = units_right_layer
 	
 	w[len(hidden_units_list)] = np.random.normal(loc=float(0), scale=std, size=(units_left_layer + 1, num_output))	# + 1 for bias
-	#w[len(hidden_units_list)] = np.random.uniform(-0.1, 0.1, (units_left_layer + 1, num_output))
 	
 	return w
-
-# input : a neural_network, an input (vector) consistent with the network
-# returns : list of output vectors for each layer of the network
-def forward(neural_net, input):
-	output_list = []
-	
-	i = np.reshape(input, (len(input), 1))
-	layer_input = np.vstack([i, [1]]).T 		 	# transpose of input vector, 
-
-	for w in neural_net:
-		layer_output = np.matmul(layer_input, w) 				# transpose of output vector
-		layer_output = sigmoid(layer_output)
-		layer_input = np.vstack([layer_output.T, [1]]).T		# append 1 for bias
-		
-		output_list.append(layer_output.T)
-
-	return output_list
 
 # input : a neural_network, an input (list of vectors) consistent with the network
 # returns : list of output vectors for each layer of the network
@@ -77,41 +49,10 @@ def forward_multi(neural_net, input):
 	return output_list
 
 # returns : gradient for each parameter
-def backward(neural_net, input, output, target_output):
-	t = np.reshape(target_output, (len(target_output), 1))
-	inp = np.reshape(input, (len(input), 1))
-
-	num_layers = len(neural_net)
-	gradient = [0]*num_layers
-	delta = [0]*num_layers
-	
-	layer_output = output[num_layers-1]
-	delta[num_layers-1] = layer_output * (1 - layer_output) * (layer_output - t)
-
-	for i in range(num_layers-2, -1, -1):
-		layer_output = output[i]
-		delta[i] = layer_output * (1 - layer_output) * np.matmul(neural_net[i+1][:-1,:], delta[i+1])
-
-	for i in range(num_layers-1, 0, -1):
-		augmented_layer_output = np.vstack([output[i-1], [1]])
-		gradient[i] = np.matmul(augmented_layer_output, delta[i].T)
-
-	augmented_layer_output = np.vstack([inp, [1]])
-	gradient[0] = np.matmul(augmented_layer_output, delta[0].T)
-
-	return gradient
-
-def column(array, k):
-	c = array[:,k]
-	return c.reshape((len(c),1))
-
-# returns : gradient for each parameter
 def backward_multi(neural_net, input, output, target_output):
 	m = len(target_output)
 	t = target_output.T
 	inp = np.hstack([np.ones((input.shape[0],1)), input]).T
-
-	#print t
 
 	num_layers = len(neural_net)
 	gradient = [0]*num_layers
@@ -123,19 +64,7 @@ def backward_multi(neural_net, input, output, target_output):
 
 	for i in range(num_layers-2, -1, -1):
 		layer_output = output[i]
-		#delta[i] = layer_output * (1 - layer_output) * np.matmul(neural_net[i+1][:-1,:], delta[i+1])
 		delta[i] = layer_output * (1 - layer_output) * (np.dot(neural_net[i+1], delta[i+1])[1:,:])
-
-	# print "**"
-	# print delta
-
-	# for k in range(m):
-	# 	for i in range(num_layers-1, 0, -1):
-	# 		augmented_layer_output = np.vstack([column(output[i-1],k), [1]])
-	# 		gradient[i] = np.matmul(augmented_layer_output, column(delta[i],k).T)
-
-	# 	augmented_layer_output = column(inp,k)
-	# 	gradient[0] = np.matmul(augmented_layer_output, column(delta[0],k).T)
 
 	z = output[0].shape[1]
 	for i in range(num_layers-1, 0, -1):
@@ -144,20 +73,7 @@ def backward_multi(neural_net, input, output, target_output):
 		gradient[i] = np.dot(delta[i],augmented_layer_output.T).T
 
 	augmented_layer_output = inp
-	#gradient[0] = np.matmul(augmented_layer_output, delta[0].T)
 	gradient[0] = np.dot(delta[0],augmented_layer_output.T).T
-
-	# gradient_list.append(gradient)
-
-	# sum_gradient = []
-	# for j in range(len(neural_net)):
-	# 	sum_gradient.append(gradient_list[0][j].copy()/m)
-
-	# for i in range(1,m):
-	# 	for j in range(len(neural_net)):
-	# 		sum_gradient[j] += gradient_list[i][j]/m
-
-	# return sum_gradient
 
 	return gradient
 
@@ -176,7 +92,7 @@ def getError(output, y):
 # input : neural network to train, x,y for training
 # returns : trained neural network
 def train(neural_net, x, y, batch_size, neta):
-	max_iterations = 50000	# max epochs
+	max_iterations = 50000	# max iterations
 	error_threshold = 0.00001
 
 	error_old = -1.0
@@ -196,41 +112,13 @@ def train(neural_net, x, y, batch_size, neta):
 
 			error_new += getError(output, y[start:start+batch_size])
 
-		# print x[start:start+batch_size]
-		# print output[len(output)-1].T
-		# print y[start:start+batch_size]
-
 		error_new /= len(y)
 		print error_new
 		#if (abs(error_old - error_new) < error_threshold and error_old >= 0.0) :
 		#	return neural_net
 		error_old = error_new	
-		print it
+
 	return neural_net
-
-# input : a neural_network, an input consistent with the network
-# returns : multi dimensional output
-def predict(neural_net, input):
-	i = np.reshape(input, (len(input), 1))
-	layer_input = np.vstack([i, [1]]).T 		 	# transpose of input vector, 
-
-	for w in neural_net:
-		layer_output = np.matmul(layer_input, w) 				# transpose of output vector
-		layer_output = sigmoid(layer_output)
-		layer_input = np.vstack([layer_output.T, [1]]).T		# append 1 for bias
-
-	index = np.argmax(layer_output)
-	layer_output = [0]*layer_output.shape[1]
-	layer_output[index] = 1
-
-	return layer_output
-
-def predict_multi(neural_net, X):
-	predicted = []
-	for x1 in X:
-		predicted.append(predict(neural_net, x1))
-
-	return predicted
 
 def predict1(neural_net,X):
 	n = len(neural_net)
@@ -274,83 +162,6 @@ def accuracy(Y_inp, Y_pred):
 		if Y_inp[i] == Y_pred[i]:
 			count += 1
 	return float(count)/len(Y_inp)
-
-# n = create_network(2, [1], 2)
-# print n
-# input = [[1.0,1.0], [1.0,1.0]]
-# #print np.array([[0.5], [1]])
-# #o = forward(n, input)
-# #print o
-# #print backward(n, input, o, [1.0])
-# print "****"
-# a = forward_multi(n, input)
-# print a
-
-# n1 = train(n, np.array(input), np.array([[0.0, 1.0], [0.0, 1.0]]), 2, 0.1)
-# print n1
-# print 
-# print "---------"
-# print backward_multi(n, np.array(input), a, np.array([[0.0, 1.0], [0.0, 1.0]]))
-
-# print predict(n1, input[0])
-# o = forward(n, [[1,2],[1,3]])
-# print np.array(o)
-
-# n = [np.array([[0.15,0.25], [0.20,0.30], [0.35,0.35]]), np.array([[0.40,0.50], [0.45,0.55], [0.60,0.60]])]
-# y = [[0.01,0.99]]
-# x = [[0.05,0.10]]
-# print n
-# o = forward_multi(n, x)
-# print o
-# print getError(o, np.array(y))/2.0
-# b = backward_multi(n, np.array(x), o, np.array(y))
-# print b 
-# neta = 0.5
-# neta_gradient = [neta*g for g in b]
-# neural_net = [n1-g for (n1,g) in zip(n, neta_gradient)]
-# o = forward_multi(neural_net, x)
-# print o
-# print getError(o, np.array(y))/2.0
-# print "-------"
-# print neural_net
-# train(n, np.array(x), np.array(y), 1, 0.5)
-
-# n = [np.array([[0.15,0.25], [0.20,0.30], [0.35,0.35]]), np.array([[0.40,0.5], [0.45,0.55], [0.60,0.60]])]
-# y = [[0.01,0.99]]
-# x = [[0.05,0.10]]
-# print n
-# o = forward_multi(n, x)
-# print o
-# print getError(o, np.array(y))/2.0
-# b = backward_multi(n, np.array(x), o, np.array(y))
-# print b 
-# neta = 0.5
-# neta_gradient = [neta*g for g in b]
-# neural_net = [n1-g for (n1,g) in zip(n, neta_gradient)]
-# print neural_net
-# o = forward_multi(neural_net, x)
-# print getError(o, np.array(y))/2.0
-# print train(n, np.array(x), np.array(y), 1, 0.5)
-
-# n = [np.array([[ 1.24737338,  0.28295388,  0.69207227],[ 1.58455078,  1.32056292, -0.69103982]]).T, 
-# 	np.array([[ 0.67181396, -0.10702571, -0.07298675],[ 0.29033699,  0.10185419,  1.02832666]]).T]
-# y = [[1,0]]
-# x = [[1,2]]
-# print n
-# train(n, np.array(x), np.array(y), 1, 0.1)
-# o = forward_multi(n, x)
-# print o
-# print getError(o, np.array(y))/2.0
-# b = backward_multi(n, np.array(x), o, np.array(y))
-# print b 
-# neta = 0.1
-# neta_gradient = [neta*g for g in b]
-# neural_net = [n1-g for (n1,g) in zip(n, neta_gradient)]
-# print neural_net
-
-# o = forward_multi(neural_net, x)
-# print getError(o, np.array(y))/2.0
-# print train(n, np.array(x), np.array(y), 1, 0.5)
 
 #################################
 # I/O ###########################
@@ -402,16 +213,8 @@ X_Tr = preprocessing.scale(X_Tr)
 net = create_network(85, [25], 10)
 trained_net = train(net, X_Tr, Y_Tr, 100, 0.1)
 
-#Y_Tr_predict = predict_multi(trained_net, X_Tr)
 Y_Tr_predict = predict1(trained_net, X_Tr)
-#Y_Te_predict = predict_multi(trained_net, X_Te)
+#Y_Te_predict = predict1(trained_net, X_Te)
 
-print(accuracy(dfTr.iloc[:,10:].values, Y_Tr_predict))
-#print "Training accuracy = ", getAccuracy(Y_Tr_predict, Y_Tr)*100
-#print "Testing accuracy = ", getAccuracy(Y_Te_predict, Y_Te)*100
-
-# from sklearn.neural_network import MLPClassifier
-# clf = MLPClassifier(hidden_layer_sizes=(20,), activation='logistic', solver='sgd', alpha=0.0, batch_size=128, learning_rate_init=0.1, max_iter=100, tol=0, random_state=1)
-# print clf.get_params()
-# clf.fit(X_Tr, Y_Tr) 
-# print getAccuracy(clf.predict(X_Tr), Y_Tr)
+print "training accuracy = ", accuracy(dfTr.iloc[:,10:].values, Y_Tr_predict)
+#print "testing accuracy = ", accuracy(dfTe.iloc[:,10:].values, Y_Te_predict)
